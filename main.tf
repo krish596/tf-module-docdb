6b18e2909466cedfb4a89f3500706249ede68d99
Chimtamreddy <kr7348202@gmail.com>
2024-08-18 08:54:48 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-# resource "aws_docdb_subnet_group" "main" {
-#   name       = "${local.name_prefix}-subnet-group"
-#   subnet_ids = var.subnet_ids
-#
-#   tags = merge(local.tags, {Name = "${local.name_prefix}-subnet-group"})
-# }
+resource "aws_docdb_subnet_group" "main" {
+  name       = "${local.name_prefix}-subnet-group"
+  subnet_ids = var.subnet_ids
+
+  tags = merge(local.tags, {Name = "${local.name_prefix}-subnet-group"})
+}
 #
 # resource "aws_docdb_cluster" "docdb" {
 #   cluster_identifier      = "my-docdb-cluster"
